6c14a515d1bab8e5474764000e4d0b434320d39e
ユーザー名 <luck@imai.am>
2025-08-22 06:45:53 +0900

    不要なファイルの削除

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,8 +1,6 @@
 export { Colors } from './Colors';
-export { Layout, Styles } from './Styles';
 export {
   EVENT_STATUS_TABS,
   EVENT_PURPOSE_LABELS,
   EMPTY_STATE_MESSAGES,
 } from './EventConstants';
-export { StatusIcon } from './StatusIcons';
